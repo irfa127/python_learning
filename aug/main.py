7b2b0e114b41a91699240eec65446f7355d704d7
irfa127 <iirfananasrin38@gmail.com>
2025-10-09 13:42:57 +0530

    change the folder name and removing hastag

--- aug/main.py
+++ aug/main.py
@@ -1,36 +1,36 @@
-# #radius input from the user
-# radius = input("Enter the radius of the cylinder : ")
-# print("you have entered radius as =",radius)
-# print("Enter the height of the cylinder")
-# height=input()
+#radius input from the user
+radius = input("Enter the radius of the cylinder : ")
+print("you have entered radius as =",radius)
+print("Enter the height of the cylinder")
+height=input()
 
-# print("Cylinder dimensions are : ",radius,height)
+print("Cylinder dimensions are : ",radius,height)
 
-# print(type(radius))
-# print(type(height))
+print(type(radius))
+print(type(height))
 
-# r = int(radius)
-# h = int(height)
-# pi = 3.14
+r = int(radius)
+h = int(height)
+pi = 3.14
 
-# v = pi * r * r * h
-# print(v)
+v = pi * r * r * h
+print(v)
 
-# #volume
+#volume
 
-# #v = pi * radius * radius * height
-# #print(v)
-
-
-
-# c = input()
-# c = int (c)
-# print(273*15+c)
+#v = pi * radius * radius * height
+#print(v)
 
 
-# a=input()
-# a=int(a)
 
-# print(1.73**a)
+c = input()
+c = int (c)
+print(273*15+c)
+
+
+a=input()
+a=int(a)
+
+print(1.73**a)
 
 
--- aug/maths.py
+++ aug/maths.py
@@ -4,4 +4,4 @@ print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
-print(a % b)ww
+print(a % b)
